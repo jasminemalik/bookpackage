43de9e9be1e2fe46ecedd2fe1865194e83180140
jasminemalik <90645321+jasminemalik@users.noreply.github.com>
2022-07-12 02:02:44 -0400

    Update demo.py

--- bookpy/demo.py
+++ bookpy/demo.py
@@ -4,5 +4,3 @@ from booklover import BookLover
 jaslibrary3 = BookLover('jas2','jas.com','mys')
 jaslibrary3.add_book('test1',1)
 jaslibrary3.num_books_read()
-jaslibrary3.add_book('test5',5)
-jaslibrary3.fav_books()
